09bda2d9a5bb3e3aed5ace2a986374bfdb9e17b2
Viktor B <viktor.bernstein@dialexa.com>
2023-08-04 16:13:55 -0400

    Adds unit tests and the word list

---
+++ utils.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+)
+
+func IsAnagram(s1 string, s2 string) bool {
+    if (len(s1) != len(s2)) {
+	return false 
+    }
+
+    var letters map[rune]uint8 = make(map[rune]uint8)
+
+    for _,letter := range s1 {
+	count, ok := letters[letter]
+	if (ok) { /* Letter is already in map */
+	    letters[letter] = count + 1
+	} else {
+	    letters[letter] = 1
+	}
+    } 
+
+    for _,letter := range s2 {
+	count, ok := letters[letter]
+	if (ok) {
+	    if (count == 0) {
+	        return false /* No more letters remaining */
+	    }
+	    letters[letter] = count - 1
+	} else {
+	    return false /* This letter is not in s1 */
+	}
+    }
+    return true
+}
+
+func Find(space []string, target string) bool {
+    var high = len(space) - 1
+    var low = 0
+    var mid = high / 2
+
+    for low <= high {
+        var word = space[mid]
+	if (target == word) {
+	    return true
+	} else if (word < target) {
+	    low = mid + 1
+	} else {
+	    high = mid - 1
+	}	
+	mid = (high + low) / 2
+    }
+    return false
+}
+
+/* TODO: Return an error object as well; offload error handling to caller`*/
+func GetWords() []string {
+    filename := "words"
+    file, err := os.Open(filename)
+    if err != nil {
+	log.Fatal(err)
+    }
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+ 	lines = append(lines, scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+	log.Fatal(err)
+    }
+    return lines
+}
---
+++ utils_test.go
@@ -0,0 +1,69 @@
+package main
+
+import "testing"
+
+func TestIsAnagram_unequal_length(t *testing.T) {
+    ana := IsAnagram("salve", "lol")
+    if ana {
+        t.Fatal("Unequal length words aren't anagrams!")
+    }
+}
+
+func TestIsAnagram_missing_letter(t *testing.T) {
+    ana := IsAnagram("salve", "seals")
+    if ana {
+        t.Fatal("These words aren't anagrams!")
+    }
+}
+
+func TestIsAnagram_true(t *testing.T) {
+    ana := IsAnagram("listen", "silent")
+    if !ana {
+        t.Fatal("These should be anagrams!")
+    }
+}
+
+func TestFind_empty_space(t *testing.T) {
+    searchSpace := []string{}
+    target := "fordl"
+    found := Find(searchSpace, target) 
+    if found {
+        t.Fatal("Search space is empty!")
+    }
+}
+
+func TestFind_not_in_space(t *testing.T) {
+    searchSpace := []string{"ab","bc", "cd","de","fgh","fordle","lol"}
+    target := "ford"
+    found := Find(searchSpace, target) 
+    if found {
+        t.Fatal("Word not in space!")
+    }
+}
+
+func TestFind_found(t *testing.T) {
+    searchSpace := []string{"ab","bc", "cd","de","fgh","fordle","lol"}
+    target := "fordle"
+    found := Find(searchSpace, target) 
+    if !found {
+        t.Fatal("Word in space!")
+    } 
+}
+
+func TestGetWords_length(t *testing.T) {
+    words := GetWords()
+    if len(words) != 2309 {
+	t.Fatal("Unsuccessful in getting all the words")
+    }
+}
+
+func TestGetWords_isAlphabetical(t *testing.T) {
+    words := GetWords()
+    for i := 0; i < len(words) - 1; i++ {
+	cur := words[i]
+	next := words[i+1]
+	if next <= cur {
+	    t.Fatal("The words are jumbled")
+	}
+    }
+}
